08a02cc289f227f7894b18750fe75b397f775ed8
root <root@OVM.myguest.virtualbox.org>
2025-10-14 23:29:04 +0300

    no oslogin

--- nodes/main.tf
+++ nodes/main.tf
@@ -17,9 +17,9 @@ data "terraform_remote_state" "infrastructure" {
   backend = "s3"
 
   config = {
-    bucket = "tf-state-nkh-d"
-    key    = "infrastructure.tfstate"
-    region = "ru-central1"
+    bucket                     = "tf-state-nkh-d"
+    key                        = "infrastructure.tfstate"
+    region                     = "ru-central1"
     endpoints = {
       s3 = "https://storage.yandexcloud.net"
     }
@@ -52,30 +52,32 @@ locals {
       preemptible = false
     }
     worker1 = {
-      hostname    = "worker1"
-      zone        = "ru-central1-b"
-      subnet      = "subnet-b"
-      cores       = 2
-      memory      = 4
-      preemptible = true
+      hostname      = "worker1"
+      zone          = "ru-central1-b"
+      subnet        = "subnet-b"
+      cores         = 2
+      memory        = 4
+      preemptible   = true
       core_fraction = 20
     }
     worker2 = {
-      hostname    = "worker2"
-      zone        = "ru-central1-d"
-      subnet      = "subnet-d"
-      cores       = 2
-      memory      = 4
-      preemptible = true
+      hostname      = "worker2"
+      zone          = "ru-central1-d"
+      subnet        = "subnet-d"
+      cores         = 2
+      memory        = 4
+      preemptible   = true
       core_fraction = 20
-      platform_id = "standard-v3"
+      platform_id   = "standard-v3"
     }
   }
+
   subnets = {
     subnet-a = data.yandex_vpc_subnet.subnet-a
     subnet-b = data.yandex_vpc_subnet.subnet-b
     subnet-d = data.yandex_vpc_subnet.subnet-d
   }
+
   subnet_cidrs = {
     subnet-a = data.terraform_remote_state.infrastructure.outputs.subnet_a_cidr
     subnet-b = data.terraform_remote_state.infrastructure.outputs.subnet_b_cidr
@@ -93,6 +95,7 @@ resource "yandex_compute_instance" "vm" {
   resources {
     cores  = each.value.cores
     memory = each.value.memory
+    core_fraction = lookup(each.value, "core_fraction", 100)
   }
 
   scheduling_policy {
@@ -107,14 +110,24 @@ resource "yandex_compute_instance" "vm" {
   }
 
   network_interface {
-    subnet_id = local.subnets[each.value.subnet].id
-    nat       = true
+    subnet_id          = local.subnets[each.value.subnet].id
+    nat                = true
     security_group_ids = [data.terraform_remote_state.infrastructure.outputs.security_group_id]
   }
 
   metadata = {
-    ssh-keys = "ubuntu:${file(var.ssh_public_key_path)}"
-    enable-oslogin   = "false"
-    serial-port-enable = "0"
+    ssh-keys           = "ubuntu:${file(var.ssh_public_key_path)}"
+    enable-oslogin     = "false"
+    serial-port-enable = "1"
+
+    user-data = <<EOF
+users:
+  - default
+  - name: ubuntu
+    ssh-authorized-keys:
+      - ${file(var.ssh_public_key_path)}
+    sudo: ["ALL=(ALL) NOPASSWD:ALL"]
+    shell: /bin/bash
+EOF
   }
 }
